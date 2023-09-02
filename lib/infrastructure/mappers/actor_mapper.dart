8d6eb4a485c2ccc1e6c033d435da0c5ea8920090
Jonathan Pillajo <jonathan.pillajo@hotmail.com>
2023-09-02 18:53:47 -0500

    Películas individuales y actores terminado

---
+++ lib/infrastructure/mappers/actor_mapper.dart
@@ -0,0 +1,12 @@
+import 'package:cinemapedia/domain/entities/actor.dart';
+import 'package:cinemapedia/infrastructure/models/moviedb/credits_response.dart';
+
+class ActorMapper {
+  static Actor castToEntity(Cast cast) => Actor(
+      id: cast.id,
+      name: cast.name,
+      profilePath: cast.profilePath != null
+          ? 'https://image.tmdb.org/t/p/w500/${cast.profilePath}'
+          : 'https://cdn-icons-png.flaticon.com/512/5436/5436149.png',
+      character: cast.character);
+}
